c88769bfaccc1f085ef57f6d922ea66dbed5d3c4
felkinsio <143757815+felkinsio@users.noreply.github.com>
2025-12-03 11:29:15 -0500

    Update Main to use
    Refactor Veterinaria initialization and animal addition

--- Main.java
+++ Main.java
@@ -1,13 +1,18 @@
-import Veterinaria.*;
+import veterinaria.Animal;
+import veterinaria.Perro;
+import veterinaria.Veterinaria;
+
 public class Main {
     public static void main(String[] args) {
-        Animal[] animales = new Animal[2];
         
-        animales[0] = new Perro("Firulais", "Café", "Croquetas");
-        animales[1] = new Perro("Max", "Negro", "Pollo");
-        
-        Veterinaria veterinaria = new Veterinaria("El perro Feliz", animales);
+        Veterinaria veterinaria = new Veterinaria("El Perro Feliz");
 
+        Animal perro1 = new Perro("Firulais", "Café", "Croquetas");
+        Animal perro2 = new Perro("Max", "Negro", "Pollo");
+
+        veterinaria.agregarAnimal(perro1);
+        veterinaria.agregarAnimal(perro2);
+        
         veterinaria.mostrarAnimales();
         veterinaria.tratarAnimales();
         veterinaria.mostrarAnimales();
